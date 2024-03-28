b2416296ca16462efc2c11b583ebacf94a91f613
Bapan Ghosh <bapanghosh166@gmail.com>
2024-03-28 22:33:58 +0530

    changes

--- Javascript by akshay saini/HigherOrderFunction.js
+++ Javascript by akshay saini/HigherOrderFunction.js
@@ -1,2 +1,2 @@
-/*  A function that receives another function as an argument or that returns a 
+/*  A function that receives another function as an parameter or that returns a 
     new function or both is called Higher-order function */
